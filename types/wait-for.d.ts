d3b3852a8d47b29d1bcfd0a489f6cdc8c7f7c42a
Stephan de Vries <stephan281094@users.noreply.github.com>
2024-07-16 13:31:00 +0200

    Add documentation to types

--- types/wait-for.d.ts
+++ types/wait-for.d.ts
@@ -1,6 +1,15 @@
 export interface waitForOptions {
+  /**
+   * Restrict your query using a container, defaults to `document.body`.
+   */
   container?: HTMLElement
+  /**
+   * Maximum wait time in milliseconds, defaults to 1000ms.
+   */
   timeout?: number
+  /**
+   * Interval in milliseconds, defaults to 50ms.
+   */
   interval?: number
   onTimeout?: (error: Error) => Error
   mutationObserverOptions?: MutationObserverInit
